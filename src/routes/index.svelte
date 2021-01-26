f7d7e343091278a83bad2a4c2318c1b924a6415f
Anders Olsen <andersnolsen@gmail.com>
2021-01-26 14:42:44 +0100

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<style>
+	.main {
+		text-align: center;
+	}
+</style>
+
+<script context="module">
+	import { siteTitle } from '../stores/_config.js';
+	export async function preload() {
+
+		const res = await this.fetch(`undefined.json`);
+		const article = await res.json();
+
+		return { article, siteTitle };
+	}
+</script>
+
+<script>
+	export let article;
+	export let siteTitle;
+</script>
+
+<svelte:head>
+	<title>{siteTitle}</title>
+</svelte:head>
+
+<div class="main">
+	{@html article.html}
+</div>
